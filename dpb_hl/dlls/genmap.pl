6e0370c98796bd34640dcc6dd757043260ba66f0
Jorge Rodriguez <bs.vino@gmail.com>
2005-12-03 21:10:54 +0000

    Moved the old DPB source for HL1 over to a tag for reference.

--- dpb_hl/dlls/genmap.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-print "{\n\tglobal:\n\t\tGiveFnptrsToDll;\n\t\tGetEntityAPI;\n\t\tGetEntityAPI2;\n\t\tServer_GetBlendingInterface;\n";
-while(<STDIN>) {
-	chomp;
-	/\( *(.*),/;
-	print "\t\t" . $1 . ";\n";
-}
-print "\tlocal: *;\n};\n"
