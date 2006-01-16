57c16d2cb27c8749457d580d031d724dc052a31a
Ryan Goodrich <rygoody@9921e2bc-0907-0410-925d-d977b3c218c2>
2006-01-16 03:30:20 +0000

    dpb_begin.wmf with pine tree tests in it

---
+++ content/source/maps/dpb_begin.bat
@@ -0,0 +1,5 @@
+"%sourcesdk%\bin\vbsp.exe" -game "c:\program files\steam\SteamApps\SourceMods\dpb" "C:\Projects\DPB\content\source\maps\dpb_begin"
+"%sourcesdk%\bin\vvis.exe" -game "c:\program files\steam\SteamApps\SourceMods\dpb" "C:\Projects\DPB\content\source\maps\dpb_begin"
+"%sourcesdk%\bin\vrad.exe" -game "c:\program files\steam\SteamApps\SourceMods\dpb" "C:\Projects\DPB\content\source\maps\dpb_begin"
+copy "C:\Projects\DPB\content\source\maps\dpb_begin.bsp" "c:\program files\steam\SteamApps\SourceMods\dpb\maps\dpb_begin.bsp"
+pause
