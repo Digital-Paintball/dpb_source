26140ae46081b36b9e59b39e1276f4190abd79b1
John Echeveste <jmechy@gmail.com>
2006-02-01 06:05:20 +0000

    Added a compiled folder for finished blazer files fixed the bat and QC file for the blazer so that they now compile Added an animations folder to store my .max files Last update for the night, i promise.  :)

--- content/source/ModelsRC/weapons/v_blazer/hl2-compile.bat
+++ content/source/ModelsRC/weapons/v_blazer/hl2-compile.bat
@@ -1,4 +1,4 @@
 ::had to edit this to get it to run on my machine
-@echo off
-%sourcesdk%\bin\studiomdl.exe -game "D:\Program Files\Steam\SteamApps\SourceMods\dpb" %1
+::@echo off
+"%sourcesdk%\bin\studiomdl.exe" -game "D:\Program Files\Steam\SteamApps\SourceMods\dpb" %1
 pause
