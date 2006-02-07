4f8dbdf8c1b4dc3db6709ed0b513196a7df19226
Jorge Rodriguez <bs.vino@gmail.com>
2006-02-07 17:53:06 +0000

    Build release with debug info.

--- build/makedpb.bat
+++ build/makedpb.bat
@@ -87,7 +87,7 @@ cd %build%
 
 @echo on
 
-cmake -G"NMake Makefiles" -DCMAKE_INSTALL_PREFIX:STRING=%install% -DCMAKE_BUILD_TYPE:STRING=Release "%source%"
+cmake -G"NMake Makefiles" -DCMAKE_INSTALL_PREFIX:STRING=%install% -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo "%source%"
 ::@if not errorlevel 0 goto builderror
 
 nmake
